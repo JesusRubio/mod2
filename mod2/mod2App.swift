05e811a7d5c799f77cf8239e49cf068b3cabcd78
Jesus Rubio <jrubio@lithographix.com>
2023-10-01 10:34:09 -0700

    Initial Commit

---
+++ mod2/mod2App.swift
@@ -0,0 +1,17 @@
+//
+//  mod2App.swift
+//  mod2
+//
+//  Created by Jesus Rubio on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct mod2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
